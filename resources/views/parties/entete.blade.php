616fcda8279c59f2b7c886402ff572a31cede2a0
silasmas <silasjmas@gmail.com>
2024-05-07 16:52:14 +0100

    idid

--- resources/views/parties/entete.blade.php
+++ resources/views/parties/entete.blade.php
@@ -18,10 +18,10 @@
     <title> {{config('app.name') }} | {{isset($titre)?$titre:""}}</title>
 
     <!-- favicon -->
-    <link rel="shortcut icon" href="{{ asset('assets/img/logos/favicon.png') }} ">
-    <link rel="apple-touch-icon" href="{{ asset('assets/img/logos/apple-touch-icon-57x57.png') }} ">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/img/logos/apple-touch-icon-72x72.png') }} ">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('assets/img/logos/apple-touch-icon-114x114.png') }} ">
+    <link rel="shortcut icon" href="{{ asset('assets/img/logos/three.png') }} ">
+    <link rel="apple-touch-icon" href="{{ asset('assets/img/logos/three.png') }} ">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/img/logos/three.png') }} ">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('assets/img/logos/three.png') }} ">
 
     <!-- plugins -->
     <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }} ">
